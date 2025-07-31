fca18b84c6a7f0902e0460d7c819dace4c682f0b
Noah Okada <nssokada@gmail.com>
2025-07-31 15:01:57 -0700

    update dashboard

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CompetitiveAnalysisDashboard from './components/CompetitiveAnalysisDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <CompetitiveAnalysisDashboard />
+    </div>
+  );
+}
+
+export default App;
